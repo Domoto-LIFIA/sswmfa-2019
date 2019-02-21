99f8ff0cd3b5e5cc22aae64372c7bc42e6c4c71f
aCastelli95 <alan.castelli@gba.gob.ar>
2019-02-21 00:05:15 -0300

    EStablecer un criterio para las BD asi no importamos monton de veces la secuencia del plugin extensionIAW

--- models/db.js
+++ models/db.js
@@ -1,9 +1,15 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('sqlite:sswmfa.db');
 
-const Descripciones = sequelize.define('Descripciones', {
+const TelevisoresListados = sequelize.define('TelevisoresListados', {
  id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-  estrellas: { type: Sequelize.STRING, allowNull: false }
+  titulo: { type: Sequelize.STRING, allowNull: false, unique: true },
+  precio: { type: Sequelize.STRING, allowNull: false }
+});
+
+const COmentario = sequelize.define('COmentario', {
+ id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+  TextoComentario: { type: Sequelize.STRING, allowNull: false, unique: true }
 });
 
 module.exports = sequelize;

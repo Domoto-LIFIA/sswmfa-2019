d4da21dea28e4ac9dc756688668f72f32265bc1a
Alan Castelli <alan.castelli@fluxit.com.ar>
2019-03-19 21:33:19 -0300

    TelevisoresListado y Comentarios

--- models/db.js
+++ models/db.js
@@ -3,19 +3,6 @@ const sequelize = new Sequelize('sqlite:sswmfa.db');
 
 const TelevisoresListado = sequelize.define('TelevisoresListado', {
  id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-<<<<<<< HEAD
-  titulo: { type: Sequelize.STRING },
-  precio: { type: Sequelize.STRING, allowNull: false }
-});
-
-const COmentarios = sequelize.define('COmentarios', {
- id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-  TextoComentario: { type: Sequelize.STRING }
-});
-
-TelevisoresListado.hasMany(COmentarios);
-
-=======
   Titulo: { type: Sequelize.STRING, allowNull: false, unique: true },
   precio: { type: Sequelize.STRING, allowNull: false }
 });
@@ -26,5 +13,4 @@ const Comentarios = sequelize.define('Comentarios', {
 });
 
 TelevisoresListados.hasMany(Comentarios);
->>>>>>> master
 module.exports = sequelize;
